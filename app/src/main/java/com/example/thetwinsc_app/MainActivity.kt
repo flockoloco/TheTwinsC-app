800b1a5ab7c2f63cc4cf1959ae59fcce8de252ec
flockoloco <art913@hotmail.com>
2020-12-03 21:13:04 +0000

    worked?

--- app/src/main/java/com/example/thetwinsc_app/MainActivity.kt
+++ app/src/main/java/com/example/thetwinsc_app/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.thetwinsc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_menu)
+
+        findViewById<MaterialToolbar>(R.id.topAppBar).setNavigationOnClickListener{
+            //handle the navigation icon press
+        }
+
+        findViewById<MaterialToolbar>(R.id.topAppBar).setOnMenuItemClickListener{menuItem -> when (menuItem.itemId){
+            R.id.search -> {
+                // Handle search icon press
+                true
+            }
+            else -> false
+        }}
     }
 }
