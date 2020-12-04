989cd055b36819c748c0b5fa66856b1c3aa66d0e
flockoloco <art913@hotmail.com>
2020-12-04 16:27:17 +0000

    blablblaablba

--- app/src/main/java/com/example/thetwinsc_app/MainActivity.kt
+++ app/src/main/java/com/example/thetwinsc_app/MainActivity.kt
@@ -2,23 +2,49 @@ package com.example.thetwinsc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_menu)
 
-        findViewById<MaterialToolbar>(R.id.topAppBar).setNavigationOnClickListener{
-            //handle the navigation icon press
+        val drawer = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val navView = findViewById<NavigationView>(R.id.nav_View)
+        val topBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+
+
+        topBar.setNavigationOnClickListener {
+            toggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+            drawer.addDrawerListener(toggle)
+            toggle.syncState()
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         }
 
-        findViewById<MaterialToolbar>(R.id.topAppBar).setOnMenuItemClickListener{menuItem -> when (menuItem.itemId){
-            R.id.search -> {
-                // Handle search icon press
-                true
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_Anvil -> Toast.makeText(applicationContext, "AAAAAAAAAAAAAAAAAAA", Toast.LENGTH_LONG).show()
             }
-            else -> false
-        }}
+            true
+        }
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
